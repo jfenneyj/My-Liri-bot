8df0e38747128a6eff7c0a011082e158ad9e1875
jfenneyj <jfenney@leadtomarket.com>
2018-07-03 12:37:34 -0700

    updated js file

--- liri.js
+++ liri.js
@@ -1,4 +1,4 @@
-var dotenv = require("dotenv").confiq();
+require("dotenv").confiq();
 
 //packages
 var keys = require("./keys.js");
@@ -32,15 +32,29 @@ switch (liriRtn){
     doWhatItSays();
     break;
 
-    // instructions on how to use liri
-    default: console.log("\n" + "type andy command after 'node liri.js': " + "\n" + "my-tweets" + "\n" +
-    "spotify-this-song 'any song title' " + "\n" +
-    "movie-this 'any movie title' " + "\n" +
-    "do-what-it-says " + "\n" +
-    "Use quotes for multiword titles!");
+     // instructions for first-time user lurking around on the command line
+     default: console.log("\n" + "type any command after 'node liri.js': " + "\n" +
+     "my-tweets" + "\n" +
+     "spotify-this-song 'any song title' " + "\n" +
+     "movie-this 'any movie title' " + "\n" +
+     "do-what-it-says " + "\n" +
+     "Use quotes for multiword titles!");
+    
 };
 
-function myTweets(){
-    console.log()
-}
+//tweets function
+function myTweets() {
+    var params = { screen_name: 'jayinmarketing' };
+    client.get('statuses/user_timeline', params, function (error, tweets, response) {
+        if (!error) {
+            for (var i = 0; i < tweets.length; i++) {
+
+                console.log(tweets[i].text);
+            };
+        } else {
+            console.log("error: " + err);
+            return;
+        };
+    });
+};
 
